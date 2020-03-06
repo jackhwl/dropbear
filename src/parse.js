4f05b96abc179ad65da7dd6773d132c43bd9b038
Wenlin Huang <whuang@viglobal.com>
2020-03-06 09:27:30 -0500

    parenthesize

--- src/parse.js
+++ src/parse.js
@@ -1,34 +1,45 @@
-const { isOpeningParenthesis, isClosingParenthesis } = require('./identify');
-const { specialForms } = require('./special-forms');
-const { peek, pop } = require('./utilities');
+const { isOpeningParenthesis, isClosingParenthesis } = require('./identify')
+const { specialForms } = require('./special-forms')
+const { peek, pop } = require('./utilities')
 
 const parenthesize = tokens => {
-  return tokens;
-};
+  const token = pop(tokens)
+
+  if (isOpeningParenthesis(token.value)) {
+    const expression = []
+
+    while (!isClosingParenthesis(token.value)) {
+      expression.push(parenthesize(token))
+    }
+  }
+
+  pop(tokens)
+  return expression
+}
 
 const parse = tokens => {
-  const token = pop(tokens);
+  const token = pop(tokens)
 
   if (token.type === 'Number') {
     return {
       type: 'NumericLiteral',
       value: token.value,
-    };
+    }
   }
 
   if (token.type === 'String') {
     return {
       type: 'StringLiteral',
       value: token.value,
-    };
+    }
   }
 
   if (token.type === 'Name') {
     return {
       type: 'Identifier',
       name: token.value,
-    };
+    }
   }
-};
+}
 
-module.exports = { parse: tokens => parse(parenthesize(tokens)) };
+module.exports = { parse: tokens => parse(parenthesize(tokens)) }
