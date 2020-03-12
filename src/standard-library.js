858ae64aa33d8e594a0d723240e1a42dd8f4e9cb
Wenlin Huang <jackhwl@hotmail.com>
2020-03-11 20:11:13 -0400

    adding-to-the-standard-library

--- src/standard-library.js
+++ src/standard-library.js
@@ -1,11 +1,12 @@
-const all = fn => (...list) => list.reduce(fn);
+const all = fn => (...list) => list.reduce(fn)
 
-const add = all((a, b) => a + b);
-const subtract = all((a, b) => a - b);
-const multiply = all((a, b) => a * b);
-const divide = all((a, b) => a / b);
-const modulo = all((a, b) => a % b);
-const log = console.log;
+const add = all((a, b) => a + b)
+const subtract = all((a, b) => a - b)
+const multiply = all((a, b) => a * b)
+const divide = all((a, b) => a / b)
+const modulo = all((a, b) => a % b)
+//const max = all((a, b) => (a > b ? a : b))
+const log = console.log
 
 const environment = {
   add,
@@ -13,8 +14,11 @@ const environment = {
   multiply,
   divide,
   modulo,
+  max(...args) {
+    return Math.max(...args)
+  },
   log,
   pi: Math.PI,
-};
+}
 
-module.exports = { environment };
+module.exports = { environment }
