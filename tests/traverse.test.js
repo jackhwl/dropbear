349d3384367c61ca1ad2a618ee596907e6d771cf
Wenlin Huang <jackhwl@hotmail.com>
2020-03-13 21:49:47 -0400

    testing-traverse

--- tests/traverse.test.js
+++ tests/traverse.test.js
@@ -1,7 +1,7 @@
-const { traverse } = require('../src/traverse');
+const { traverse } = require('../src/traverse')
 
 describe(traverse, () => {
-  it.skip('should travel to all the nodes in the tree and reverse the math', () => {
+  it('should travel to all the nodes in the tree and reverse the math', () => {
     const ast = {
       type: 'CallExpression',
       name: 'add',
@@ -9,29 +9,29 @@ describe(traverse, () => {
         { type: 'NumericLiteral', value: 12 },
         { type: 'NumericLiteral', value: 6 },
       ],
-    };
+    }
 
     const visitor = {
       CallExpression: {
         enter({ node }) {
           if (node.name === 'add') {
-            node.name = 'subtract';
+            node.name = 'subtract'
           }
         },
       },
       NumericLiteral: {
         exit({ node }) {
-          node.value = node.value * 2;
+          node.value = node.value * 2
         },
       },
-    };
+    }
 
-    traverse(ast, visitor);
+    traverse(ast, visitor)
 
-    expect(ast.name).toBe('subtract');
-  });
+    expect(ast.name).toBe('subtract')
+  })
 
-  it.skip('should travel to all the nodes in the tree and double all of the numbers', () => {
+  it('should travel to all the nodes in the tree and double all of the numbers', () => {
     const ast = {
       type: 'CallExpression',
       name: 'add',
@@ -39,19 +39,19 @@ describe(traverse, () => {
         { type: 'NumericLiteral', value: 12 },
         { type: 'NumericLiteral', value: 6 },
       ],
-    };
+    }
 
     const visitor = {
       NumericLiteral: {
         exit({ node }) {
-          node.value = node.value * 2;
+          node.value = node.value * 2
         },
       },
-    };
+    }
 
-    traverse(ast, visitor);
+    traverse(ast, visitor)
 
-    expect(ast.arguments[0].value).toBe(24);
-    expect(ast.arguments[1].value).toBe(12);
-  });
-});
+    expect(ast.arguments[0].value).toBe(24)
+    expect(ast.arguments[1].value).toBe(12)
+  })
+})
